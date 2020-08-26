648251d8ab76dc95a44c1963f9793a5cbe965ab9
Alexander Dolokov <alexander.dolokov@gmail.com>
2020-08-26 13:47:51 +0200

    added randommaps for augmentation

--- keypoint_detection/heatmap_drawing.py
+++ keypoint_detection/heatmap_drawing.py
@@ -132,11 +132,16 @@ def write_mp(frame_data):
             newW = int(_W * float(frame_data['max_height']) / _H)
             vis = cv.resize(vis,(newW,frame_data['max_height']))
 
-        vis_path = os.path.join(frame_data['dst_dir'],mode,'%s.png' % frame_data['frame_idx'] )
+        name = frame_data['frame_idx']
+        if frame_data['random_maps']:
+            name += '--'+'_'.join([str(int(np.random.uniform(1e4))) for _ in range(5)])
+            
+        vis_path = os.path.join(frame_data['dst_dir'],mode,'%s.png' % name )
+
         cv.imwrite(vis_path, vis)
     return True 
 
-def randomly_drop_visualiztions(project_id, dst_dir = '/tmp/keypoint_heatmap_vis', num = -1, horistack=True,max_height=None ):
+def randomly_drop_visualiztions(project_id, dst_dir = '/tmp/keypoint_heatmap_vis', num = -1, horistack=True,max_height=None, random_maps=False ):
     # take random frames from the db and show their labeling as gaussian heatmaps
     if not os.path.isdir(dst_dir):
         os.makedirs(dst_dir)
@@ -162,6 +167,13 @@ def randomly_drop_visualiztions(project_id, dst_dir = '/tmp/keypoint_heatmap_vis
             if not frame_idx in frame_idxs:
                 frame_idxs.append(frame_idx)
                 frame_data[frame_idx] = []
+
+            if random_maps==False:
+                # apply random noise to coordinates as augmentation 
+                KA = 400
+                keypoint_x += np.random.uniform(-KA,KA)
+                keypoint_y += np.random.uniform(-KA,KA)
+
             frame_data[frame_idx].append([keypoint_name, individual_id, keypoint_x, keypoint_y])
 
     shuffle(frame_idxs)
@@ -178,7 +190,7 @@ def randomly_drop_visualiztions(project_id, dst_dir = '/tmp/keypoint_heatmap_vis
     with Pool(processes=os.cpu_count()) as pool:
         result_objs=[]
         for i, frame_idx in enumerate(frame_data.keys()):
-            result = pool.apply_async(write_mp,({'frame_idx':frame_idx,'frame_data':frame_data[frame_idx], 'project_id':project_id, 'video_id':video_id, 'dst_dir':dst_dir, 'keypoint_names':keypoint_names, 'horistack':horistack,'max_height':max_height},))
+            result = pool.apply_async(write_mp,({'frame_idx':frame_idx,'frame_data':frame_data[frame_idx], 'project_id':project_id, 'video_id':video_id, 'random_maps':random_maps, 'dst_dir':dst_dir, 'keypoint_names':keypoint_names, 'horistack':horistack,'max_height':max_height},))
             #write_mp({'frame_idx':frame_idx,'frame_data':frame_data[frame_idx], 'project_id':project_id, 'video_id':video_id, 'dst_dir':dst_dir, 'keypoint_names':keypoint_names, 'horistack':horistack,'max_height':max_height})
             result_objs.append(result)
         results = [result.get() for result in result_objs]
