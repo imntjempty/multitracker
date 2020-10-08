smaller model. less l2 regularization. fixed test error bug. added accuracy. not training pretrained encoder. augmentations h/vflips and rotation config optionally

keypoint_detection/nets.py
@@ -15,7 +15,7 @@ def upsample(nfilters, kernel_size, strides=2, dilation = (1,1), norm_type='batc
         func(nfilters, kernel_size, strides=strides,
             dilation_rate=dilation,
             padding='same',
-            kernel_regularizer=tf.keras.regularizers.l2(0.01),
+            kernel_regularizer=tf.keras.regularizers.l2(0.0001),
             kernel_initializer=initializer))
 
     if norm_type is not None:

keypoint_detection/roi_segm.py
@@ -29,8 +29,14 @@ cce_loss = tf.keras.losses.CategoricalCrossentropy(False)
 def calc_focal_loss(ytrue,ypred):
     return tf.reduce_mean( focal_loss(ytrue, ypred))
 
-def calc_cce_loss(ytrue, pred):
+def calc_cce_loss(ytrue, ypred):
     return tf.reduce_mean( cce_loss(ytrue, ypred) )
+
+def calc_accuracy(ytrue, ypred):
+    correct_prediction = tf.equal(tf.argmax(ytrue,3),tf.argmax(ypred,3))
+    acc = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    return acc 
+
 
 def get_roi_crop_dim(project_id, video_id, Htarget):
     """
@@ -173,8 +179,8 @@ def load_roi_dataset(config,mode='train'):
     wroi = hroi#wroicomp // (1+len(config['keypoint_names'])//3)
     #print('wroi',hroi,wroicomp,wroi,'->',wroicomp//wroi)
     h = hroi #int(hroi * 0.98)
-    config['img_height'] = 224
-    config['img_width'] = 224
+    #config['img_height'] = 224
+    #config['img_width'] = 224
 
     def load_im(image_file):
         image = tf.io.read_file(image_file)
@@ -187,7 +193,7 @@ def load_roi_dataset(config,mode='train'):
         comp = tf.concat(comp,axis=2)
         comp = comp[:,:,:(3+len(config['keypoint_names']))]
         hh = h 
-        if mode == 'train':
+        if mode == 'train' and config['rotation_augmentation']:
             # random scale augmentation
             #if tf.random.uniform([]) > 0.3:
             #    hh = h + int(tf.random.uniform([],-h/10,h/10)) #h += int(tf.random.uniform([],-h/7,h/7))
@@ -217,15 +223,19 @@ def load_roi_dataset(config,mode='train'):
     if mode == 'train' and 'experiment' in config and config['experiment']=='A':
         file_list = glob(os.path.join(config['roi_dir'],mode,'*.png'))
         shuffle(file_list)
+        oldlen = len(file_list)
         file_list = file_list[:int(len(file_list) * config['data_ratio'])]
         file_list = sorted(file_list)
+        print('[*] cutting training data from %i samples to %i samples' % (oldlen, len(file_list)))
     else:
         file_list = sorted(glob(os.path.join(config['roi_dir'],mode,'*.png')))
     
+    print('[*] loaded %i samples for mode %s' % (len(file_list),mode))
+
     file_list_tf = tf.data.Dataset.from_tensor_slices(file_list)
     data = file_list_tf.map(load_im, num_parallel_calls = tf.data.experimental.AUTOTUNE).batch(config['batch_size']).prefetch(4*config['batch_size'])#.cache()
-    if mode == 'train':
-        data = data.shuffle(512)
+    #if mode == 'train':
+    data = data.shuffle(512)
     return data 
 
 def get_center(x1,y1,x2,y2,H,W,crop_dim):
@@ -271,7 +281,6 @@ def inference_heatmap(config, trained_model, frame, bounding_boxes):
     return y 
     
 def train(config):
-    config['lr'] = 1e-4
     #config['cutmix'] = False
     #config['mixup'] = True
     print('[*] config', config)
@@ -339,6 +348,7 @@ def train(config):
 
         should_test = global_step % 250 == 0
         test_losses = {'focal':0.0,'cce':0.0}
+        test_accuracy = 0.0
         if should_test:
             # test data
             nt = 0
@@ -348,13 +358,14 @@ def train(config):
                     predicted_test = tf.image.resize(predicted_test, x.shape[1:3]) 
 
                 if 'focal' in config['test_losses']:
-                    test_losses['focal'] += calc_focal_loss(y,predicted_heatmaps)
+                    test_losses['focal'] += calc_focal_loss(yt,predicted_test)
                 if 'cce' in config['test_losses']:
-                    test_losses['cce'] += calc_cce_loss(y,predicted_heatmaps)
-                
+                    test_losses['cce'] += calc_cce_loss(yt,predicted_test)
+                test_accuracy += calc_accuracy(yt,predicted_test)
                 nt += 1 
             test_losses['focal'] = test_losses['focal'] / nt
             test_losses['cce'] = test_losses['cce'] / nt
+            test_accuracy = test_accuracy / nt 
 
 
         # write summary
@@ -366,6 +377,7 @@ def train(config):
                     tf.summary.scalar("loss %s" % config['train_loss'],loss,step=global_step)
                     tf.summary.scalar('min',tf.reduce_min(predicted_heatmaps[:,:,:,:-1]),step=global_step)
                     tf.summary.scalar('max',tf.reduce_max(predicted_heatmaps[:,:,:,:-1]),step=global_step)
+                    tf.summary.scalar('accuracy', calc_accuracy(y,predicted_heatmaps))
                     im_summary('image',inp/256.)
                     for kk, keypoint_name in enumerate(config['keypoint_names']):
                         im_summary('heatmap_%s' % keypoint_name, tf.concat((tf.expand_dims(y[:,:,:,kk],axis=3), tf.expand_dims(predicted_heatmaps[:,:,:,kk],axis=3)),axis=2))
@@ -381,6 +393,7 @@ def train(config):
                     with writer_test.as_default():
                         for k,v in test_losses.items():
                             tf.summary.scalar("loss %s" % k,v,step=global_step)
+                        tf.summary.scalar('accuracy', test_accuracy,step=global_step)
                         im_summary('image',xt/256.)
                         
                         for kk, keypoint_name in enumerate(config['keypoint_names']):
@@ -415,11 +428,11 @@ def train(config):
 
         if 1:#try:
             for x,y in dataset_train:
-                if 1:
-                    if np.random.random() < 0.5:
-                        x,y = model.hflip(swaps,x,y)
-                    if np.random.random() < 0.5:
-                        x,y = model.vflip(swaps,x,y)
+            
+                if np.random.random() < 0.5 and config['hflips']:
+                    x,y = model.hflip(swaps,x,y)
+                if np.random.random() < 0.5 and config['vflips']:
+                    x,y = model.vflip(swaps,x,y)
                 if 1:        
                     # mixup augmentation
                     if np.random.random() < 0.5:

keypoint_detection/unet.py
@@ -99,8 +99,8 @@ def get_efficientB0_model(config):
         weights = None 
     
     encoder = EfficientNetB0(include_top=False, weights=weights, drop_connect_rate=0.2,input_tensor=x)
-    encoder.trainable = True 
-
+    encoder.trainable = bool(0)#True 
+    encoder.summary()
     encoded_layer_names = [
         'block1a_activation', # (112,112,32)
         'block3a_expand_activation', # (56,56,144)
@@ -111,6 +111,9 @@ def get_efficientB0_model(config):
 
     outputs = [get_decoded(config, encoder, encoded_layer_names)]
     model = tf.keras.Model(inputs=encoder.inputs, outputs=[outputs], name="EfficientUnet")
+    for l in model.layers:
+        if 'block' in l.name:
+            l.trainable=False
     model.summary()
     return model 
 
@@ -140,6 +143,11 @@ def get_efficientB6_model(config):
 
     outputs = [get_decoded(config, encoder, encoded_layer_names)]
     model = tf.keras.Model(inputs=encoder.inputs, outputs=[outputs], name="LargeEfficientUnet")
+    for l in model.layers:
+        if 'block' in l.name:
+            l.trainable=False
+            #print('[*] not training layer %s' % l.name)
+    
     model.summary()
     return model 
 
@@ -153,29 +161,33 @@ def get_decoded(config, encoder, encoded_layer_names):
     
     bf = 64
     x = encoded_layers[-1]
-    for _ in range(2):
-        x = upsample(512,3,strides=1)(x)
-        #x = tf.keras.layers.Dropout(0.5)(x)
+    if 0:
+        for _ in range(2):
+            x = upsample(512,3,strides=1)(x)
+            #x = tf.keras.layers.Dropout(0.5)(x)
 
     for i_block in range(len(encoded_layers)-1,-1,-1):
-        nf = min(512, bf * 2**i_block)
+        nf = min(256, bf * 2**i_block)
         #print('decoder',i_block,nf)
         ne = encoded_layers[i_block].shape[3]
         # append encoder layer
         e = encoded_layers[i_block]
-        f = upsample(ne,3,strides=1)(e)
-        f = tf.keras.layers.Dropout(0.5)(f)
-        f = upsample(ne,3,strides=1)(f)
-        e = e + f
+        if 0:
+            f = upsample(ne,3,strides=1)(e)
+            f = tf.keras.layers.Dropout(0.5)(f)
+            f = upsample(ne,3,strides=1)(f)
+            e = e + f
+        
         #new_size = [inputs.shape[0]/2**i_block,inputs.shape[1]/2**(1+i_block)]
         #e = tf.image.resize(e,new_size)
         x = tf.concat([x,e],axis=3)
-        
+        x = upsample(nf,1,strides=1)(x)
         x = upsample(nf,3,strides=2)(x)
-        y = upsample(nf,3,strides=1)(x)
-        y = tf.keras.layers.Dropout(0.5)(y)
-        y = upsample(nf,3,strides=1)(y)
-        x = x + y
+        if 1:
+            y = upsample(nf,3,strides=1)(x)
+            #y = tf.keras.layers.Dropout(0.5)(y)
+            y = upsample(nf,3,strides=1)(y)
+            x = x + y
     x = upsample(32,3,strides=1)(x)    
     #x = upsample(64,3,strides=1)(x)
 