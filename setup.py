393e59bd4f4f3f3e212d0610561a38c7a0ab8391
Alexander Dolokov <alexander.dolokov@gmail.com>
2022-01-18 23:46:04 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='multitracker',
+      version='0.1',
+      description='multitracker framework with UpperBoundTracker',
+      url='http://github.com/dolokov/multitracker',
+      author='Alexander Dolokov',
+      author_email='alexander.dolokov@gmail.com',
+      license='MIT',
+      packages=['src/multitracker'],
+      zip_safe=False)
